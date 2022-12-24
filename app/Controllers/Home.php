5842f8eae288738ba5b0dc3a1254c75e2e098268
Hirude <hirudetyan@proton.me>
2022-12-24 19:33:13 +0000

    Redirect on server side

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,31 @@
 
 namespace App\Controllers;
 
+use App\Models\NotesModel;
+
 class Home extends BaseController
 {
     public function index()
     {
         return view('home');
     }
+    
+    public function view()
+    {
+        return view('home');
+    }
+    
+    public function randRedirect()
+    {
+        $model = new NotesModel();
+        $randstr = $model->randNote();
+        
+        while ($model->isNoteExists($randstr) == 'true')
+        {
+            $randstr = $model->randNote();
+        }
+        
+        return redirect()->to(base_url(). '/' . $randstr);
+    }
+
 }
