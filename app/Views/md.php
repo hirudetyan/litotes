036da1d88c59ad96907965b533f8b58c63916eb3
Hirude <hirudetyan@proton.me>
2023-01-15 18:55:36 +0000

    Fix dark mode

--- app/Views/md.php
+++ app/Views/md.php
@@ -3,6 +3,7 @@
     <head>
         <title>litotes</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="color-scheme" content="light dark">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/github-markdown-css@5.1.0/github-markdown.min.css">
         <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/dompurify@2.4.3/dist/purify.min.js"></script>
@@ -11,17 +12,23 @@
         <style>
         .markdown-body {
             box-sizing: border-box;
-	    	min-width: 200px;
-	    	max-width: 1450px;
-		    margin: 0 auto;
-	    	padding: 45px;
-	    }
+            min-width: 200px;
+            max-width: 1450px;
+            margin: 0 auto;
+            padding: 45px;
+        }
 
-	    @media (max-width: 767px) {
-	    	.markdown-body {
-	    		padding: 15px;
-		    }
-	    }
+        @media (max-width: 767px) {
+            .markdown-body {
+                padding: 15px;
+            }
+        }
+
+        @media (prefers-color-scheme: dark) {
+            body {
+                background-color: #0d1117;
+            }
+        }
         </style>
     </head>
     <body>
